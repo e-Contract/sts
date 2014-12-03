7ca986ac1ad2d18e1befce49c3fd622118516f3b
Frank Cornelis <info@e-contract.be>
2014-12-03 11:25:00 +0100

    extra acceptance tests (STS validation + eID cert validation)

--- sts-tests/src/test/java/test/integ/be/e_contract/sts/CXFSTSClientTest.java
+++ sts-tests/src/test/java/test/integ/be/e_contract/sts/CXFSTSClientTest.java
@@ -19,17 +19,28 @@
 package test.integ.be.e_contract.sts;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
@@ -44,6 +55,7 @@ import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.soap.SOAPFaultException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.cxf.Bus;
@@ -62,6 +74,18 @@ import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.trust.STSClient;
 import org.apache.cxf.wsdl11.WSDLServiceFactory;
 import org.apache.ws.security.WSPasswordCallback;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -159,6 +183,56 @@ public class CXFSTSClientTest {
 	}
 
 	@Test
+	public void testSelfSignedCertificateFails() throws Exception {
+		SpringBusFactory bf = new SpringBusFactory();
+		Bus bus = bf.createBus("cxf-https-trust-all.xml");
+		BusFactory.setDefaultBus(bus);
+		// get the JAX-WS client
+		URL wsdlLocation = CXFSTSClientTest.class
+				.getResource("/example-localhost-sts.wsdl");
+		ExampleService exampleService = new ExampleService(wsdlLocation,
+				new QName("urn:be:e-contract:sts:example", "ExampleService"));
+		ExampleServicePortType port = exampleService.getExampleServicePort();
+
+		// set the web service address on the client stub
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider
+				.getRequestContext();
+		requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+				"https://localhost/iam/example");
+
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+		X509Certificate certificate = getCertificate(privateKey, publicKey);
+		List<X509Certificate> certificates = new LinkedList<X509Certificate>();
+		certificates.add(certificate);
+
+		requestContext.put(SecurityConstants.STS_CLIENT_SOAP12_BINDING, "true");
+		requestContext.put(SecurityConstants.SIGNATURE_CRYPTO,
+				new ClientCrypto(privateKey, certificates));
+		requestContext.put(SecurityConstants.STS_TOKEN_USE_CERT_FOR_KEYINFO,
+				"true");
+		requestContext.put(SecurityConstants.SIGNATURE_USERNAME, "username");
+		requestContext.put(SecurityConstants.CALLBACK_HANDLER,
+				new ExampleSecurityPolicyCallbackHandler());
+		requestContext.put(
+				SecurityConstants.PREFER_WSMEX_OVER_STS_CLIENT_CONFIG, "true");
+
+		// invoke the web service
+		try {
+			port.echo("hello world");
+			fail();
+		} catch (SOAPFaultException e) {
+			// expected
+			assertTrue(e.getMessage().contains("security token"));
+		}
+
+		bus.shutdown(true);
+	}
+
+	@Test
 	public void testCXFSTS() throws Exception {
 		// SpringBusFactory bf = new SpringBusFactory();
 		// Bus bus = bf.createBus();
@@ -208,7 +282,14 @@ public class CXFSTSClientTest {
 		stsClient.setEnableAppliesTo(true);
 		stsClient
 				.setTokenType("http://docs.oasis-open.org/ws-sx/ws-trust/200512/RSTR/Status");
-		stsClient.validateSecurityToken(securityToken);
+		List<SecurityToken> result = stsClient
+				.validateSecurityToken(securityToken);
+		assertEquals(1, result.size());
+		SecurityToken resultSecurityToken = result.get(0);
+		LOGGER.debug("token type: {}", resultSecurityToken.getTokenType());
+		assertEquals(
+				"http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0",
+				resultSecurityToken.getTokenType());
 	}
 
 	@Test
@@ -308,5 +389,39 @@ public class CXFSTSClientTest {
 				}
 			}
 		}
+	}
+
+	private static X509Certificate getCertificate(PrivateKey privateKey,
+			PublicKey publicKey) throws Exception {
+		X500Name subjectName = new X500Name("CN=Test");
+		X500Name issuerName = subjectName; // self-signed
+		BigInteger serial = new BigInteger(128, new SecureRandom());
+		SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo
+				.getInstance(publicKey.getEncoded());
+		DateTime notBefore = new DateTime();
+		DateTime notAfter = notBefore.plusMonths(1);
+		X509v3CertificateBuilder x509v3CertificateBuilder = new X509v3CertificateBuilder(
+				issuerName, serial, notBefore.toDate(), notAfter.toDate(),
+				subjectName, publicKeyInfo);
+		AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder()
+				.find("SHA1withRSA");
+		AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder()
+				.find(sigAlgId);
+		AsymmetricKeyParameter asymmetricKeyParameter = PrivateKeyFactory
+				.createKey(privateKey.getEncoded());
+
+		ContentSigner contentSigner = new BcRSAContentSignerBuilder(sigAlgId,
+				digAlgId).build(asymmetricKeyParameter);
+		X509CertificateHolder x509CertificateHolder = x509v3CertificateBuilder
+				.build(contentSigner);
+
+		byte[] encodedCertificate = x509CertificateHolder.getEncoded();
+
+		CertificateFactory certificateFactory = CertificateFactory
+				.getInstance("X.509");
+		X509Certificate certificate = (X509Certificate) certificateFactory
+				.generateCertificate(new ByteArrayInputStream(
+						encodedCertificate));
+		return certificate;
 	}
 }
--- sts-tests/src/test/java/test/integ/be/e_contract/sts/ReleaseAcceptanceTest.java
+++ sts-tests/src/test/java/test/integ/be/e_contract/sts/ReleaseAcceptanceTest.java
@@ -19,18 +19,44 @@
 package test.integ.be.e_contract.sts;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.soap.SOAPFaultException;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.ws.security.SecurityConstants;
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.trust.STSClient;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -94,6 +120,19 @@ public class ReleaseAcceptanceTest {
 				"http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0",
 				securityToken.getTokenType());
 		LOGGER.debug("security token expires: {}", securityToken.getExpires());
+
+		// STS based validation
+		stsClient.setEnableAppliesTo(true);
+		stsClient
+				.setTokenType("http://docs.oasis-open.org/ws-sx/ws-trust/200512/RSTR/Status");
+		List<SecurityToken> result = stsClient
+				.validateSecurityToken(securityToken);
+		assertEquals(1, result.size());
+		SecurityToken resultSecurityToken = result.get(0);
+		LOGGER.debug("token type: {}", resultSecurityToken.getTokenType());
+		assertEquals(
+				"http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0",
+				resultSecurityToken.getTokenType());
 	}
 
 	/**
@@ -125,5 +164,80 @@ public class ReleaseAcceptanceTest {
 
 		String result = port.echo("hello world");
 		LOGGER.debug("result: " + result);
+	}
+
+	@Test
+	public void testSelfSignedCertificateFails() throws Exception {
+		ExampleService exampleService = new ExampleService();
+		ExampleServicePortType port = exampleService.getExampleServicePort();
+
+		// set the web service address on the client stub
+		BindingProvider bindingProvider = (BindingProvider) port;
+		Map<String, Object> requestContext = bindingProvider
+				.getRequestContext();
+		requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+				"https://www.e-contract.be/iam/example");
+
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+		X509Certificate certificate = getCertificate(privateKey, publicKey);
+		List<X509Certificate> certificates = new LinkedList<X509Certificate>();
+		certificates.add(certificate);
+
+		requestContext.put(SecurityConstants.STS_CLIENT_SOAP12_BINDING, "true");
+		requestContext.put(SecurityConstants.SIGNATURE_CRYPTO,
+				new ClientCrypto(privateKey, certificates));
+		requestContext.put(SecurityConstants.STS_TOKEN_USE_CERT_FOR_KEYINFO,
+				"true");
+		requestContext.put(SecurityConstants.SIGNATURE_USERNAME, "username");
+		requestContext.put(SecurityConstants.CALLBACK_HANDLER,
+				new ExampleSecurityPolicyCallbackHandler());
+		requestContext.put(
+				SecurityConstants.PREFER_WSMEX_OVER_STS_CLIENT_CONFIG, "true");
+
+		// invoke the web service
+		try {
+			port.echo("hello world");
+			fail();
+		} catch (SOAPFaultException e) {
+			// expected
+			assertTrue(e.getMessage().contains("security token"));
+		}
+	}
+
+	private static X509Certificate getCertificate(PrivateKey privateKey,
+			PublicKey publicKey) throws Exception {
+		X500Name subjectName = new X500Name("CN=Test");
+		X500Name issuerName = subjectName; // self-signed
+		BigInteger serial = new BigInteger(128, new SecureRandom());
+		SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfo
+				.getInstance(publicKey.getEncoded());
+		DateTime notBefore = new DateTime();
+		DateTime notAfter = notBefore.plusMonths(1);
+		X509v3CertificateBuilder x509v3CertificateBuilder = new X509v3CertificateBuilder(
+				issuerName, serial, notBefore.toDate(), notAfter.toDate(),
+				subjectName, publicKeyInfo);
+		AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder()
+				.find("SHA1withRSA");
+		AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder()
+				.find(sigAlgId);
+		AsymmetricKeyParameter asymmetricKeyParameter = PrivateKeyFactory
+				.createKey(privateKey.getEncoded());
+
+		ContentSigner contentSigner = new BcRSAContentSignerBuilder(sigAlgId,
+				digAlgId).build(asymmetricKeyParameter);
+		X509CertificateHolder x509CertificateHolder = x509v3CertificateBuilder
+				.build(contentSigner);
+
+		byte[] encodedCertificate = x509CertificateHolder.getEncoded();
+
+		CertificateFactory certificateFactory = CertificateFactory
+				.getInstance("X.509");
+		X509Certificate certificate = (X509Certificate) certificateFactory
+				.generateCertificate(new ByteArrayInputStream(
+						encodedCertificate));
+		return certificate;
 	}
 }
